03b3d04262567a0d9fd40c360960472757d44e18
Alex <alex.altamirandav@proton.me>
2024-03-18 11:59:16 -0500

    Performance Feature

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,16 @@ function App() {
     setPosts(data.posts.length < 12 ? data.basicRating.posts : 12)
 
     setData(data)
-    setLoading(false)
   }
 
   useEffect(() => {
+
+    console.log()
+
+
+
     const fetchData = async () => {
-      setLoading(true)
       const response = await fetch(`https://instagram-backend-dox.vercel.app/user/ana_d_armas`).catch((e) => {console.log(e)})
-      setLoading(false)
 
       if (response.status === 400) {return}
 
@@ -41,8 +43,8 @@ function App() {
       setPosts(data.posts.length < 12 ? data.basicRating.posts : 12)
   
       setData(data)
-      setLoading(false)
     }
+
     fetchData()
   },[])
 
@@ -56,7 +58,7 @@ function App() {
           <input type="text" placeholder='Search for someone' className="username-input" onChange={(e) => {
             setUsername(e.target.value)
           }} required />
-          <button className="sumbit-button" onClick={(e) => {
+          <button className="sumbit-button" type="submit" onClick={(e) => {
             e.preventDefault()
             fetchData()
           }}><i className='bx bx-search-alt'></i></button>
@@ -67,6 +69,11 @@ function App() {
       {data ? <MainInfo data={data} postsShow={postsShow}/> : <div className='container-info'><p className='info-text'>Hi, look for some one 👍</p></div>}
       {data && postsShow != data.basicRating.posts ? <MorePosts data={postsShow} originalData={data} setPosts={setPosts}/> : ""}
     </main>
+    <button type="button" className='display-cors-iframe' title='CORS IMAGES' onClick={() => {
+      const iframeElement = document.getElementById('my-iframe')
+      iframeElement.classList.toggle('show-iframe')
+    }}><i className='bx bxs-image-alt'></i></button>
+    <iframe src="https://cors-anywhere.herokuapp.com/corsdemo" id='my-iframe'></iframe>
   </>
 }
 
